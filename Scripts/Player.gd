b28e53160cf0d6af380594d00aa08866e61eac1a
Stanislas Ambroise <stanislas.ambroise@gmail.com>
2018-02-19 13:43:15 +0100

    WIP: Movement
    Working on basic movement

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,23 +1,73 @@
 extends KinematicBody2D
 
-export (int) var SPEED
+# Member variables
+export (float) var GRAVITY # pixels/second/second
+
+# Angle in degrees towards either side that the player can consider "floor"
+const FLOOR_ANGLE_TOLERANCE = 40
+export (int) var WALK_FORCE = 600
+export (int) var WALK_MIN_SPEED = 10
+export (int) var WALK_MAX_SPEED = 200
+const STOP_FORCE = 1300
+export (int) var JUMP_SPEED = 200
+export (float) var JUMP_MAX_AIRBORNE_TIME = 0.2
+
+const SLIDE_STOP_VELOCITY = 1.0 # one pixel/second
+const SLIDE_STOP_MIN_TRAVEL = 1.0 # one pixel
+
 var velocity = Vector2()
+var on_air_time = 100
+var jumping = false
 
-func _ready():
-	# Called every time the node is added to the scene.
-	# Initialization here
-	pass
+var prev_jump_pressed = false
 
-func _process(delta):
-	var velocity = Vector2()
+
+func _physics_process(delta):
+	# Create forces
+	var force = Vector2(0, GRAVITY)
 	
-	# Manage input
-	if Input.is_action_pressed("ui_left"):
-		velocity.x -= 1
-	if Input.is_action_pressed("ui_right"):
-		velocity.x += 1
+	var walk_left = Input.is_action_pressed("move_left")
+	var walk_right = Input.is_action_pressed("move_right")
+	var jump = Input.is_action_pressed("jump")
 	
-	if velocity.length() > 0:
-		velocity = velocity.normalized() * SPEED
+	var stop = true
 	
-	position += velocity * delta
+	if walk_left:
+		if velocity.x <= WALK_MIN_SPEED and velocity.x > -WALK_MAX_SPEED:
+			force.x -= WALK_FORCE
+			stop = false
+	elif walk_right:
+		if velocity.x >= -WALK_MIN_SPEED and velocity.x < WALK_MAX_SPEED:
+			force.x += WALK_FORCE
+			stop = false
+	
+	if stop:
+		var vsign = sign(velocity.x)
+		var vlen = abs(velocity.x)
+		
+		vlen -= STOP_FORCE * delta
+		if vlen < 0:
+			vlen = 0
+		
+		velocity.x = vlen * vsign
+	
+	# Integrate forces to velocity
+	velocity += force * delta	
+	# Integrate velocity into motion and move
+	velocity = move_and_slide(velocity, Vector2(0, -1))
+	
+	if is_on_floor():
+		on_air_time = 0
+		
+	if jumping and velocity.y > 0:
+		# If falling, no longer jumping
+		jumping = false
+	
+	if on_air_time < JUMP_MAX_AIRBORNE_TIME and jump and not prev_jump_pressed and not jumping:
+		# Jump must also be allowed to happen if the character left the floor a little bit ago.
+		# Makes controls more snappy.
+		velocity.y = -JUMP_SPEED
+		jumping = true
+	
+	on_air_time += delta
+	prev_jump_pressed = jump
