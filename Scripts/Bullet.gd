5a303b99fbc0d3df3be5ab00418cd1f5c79b699c
Stanislas Ambroise <stanislas.ambroise@gmail.com>
2018-04-20 13:14:25 +0200

    Mask & Layer + First enemy
    Rework mask and layer
    First enemy improvement: Turret can shoot

--- Scripts/Bullet.gd
+++ Scripts/Bullet.gd
@@ -1,4 +1,24 @@
 extends RigidBody2D
 
+var speed = 1000
+var velocity = Vector2()
+
+func start(pos, dir):
+	position = pos
+	rotation = dir
+	velocity = Vector2(speed, 0).rotated(dir)
+
+func _physics_process(delta):
+	position += velocity * delta
+
 func _on_Bullet_body_entered(body):
+	print(body)
 	queue_free() # Destroy the bullet
+	
+	var body_name = body.get_name()
+	if body_name == "Player":
+		get_node("/root/Main").call("game_over")
+	elif body_name == "MobTurret":
+		if body.has_method("hit_by_bullet"):
+			body.call("hit_by_bullet")
+
--- Scripts/MobTurret.gd
+++ Scripts/MobTurret.gd
@@ -3,7 +3,48 @@ extends KinematicBody2D
 export (float) var GRAVITY
 export (int) var DETECT_RADIUS  # size of the visibility circle
 export (float) var FIRE_RATE  # delay time (s) between bullets
+export (PackedScene) var BULLET
+
+var target  # who are we shooting at?
+var can_shoot = true
+var velocity = Vector2()
+
+func hit_by_bullet():
+	queue_free() # kill the mob
+
+func shoot(pos):
+	var b = BULLET.instance()
+	var a = (pos - global_position).angle()
+	b.start(global_position, a + rand_range(-0.05, 0.05))
+	get_parent().add_child(b)
+	can_shoot = false
+	$ShootTimer.start()
+
+func _ready():
+	# set the collision area's radius
+	var shape = CircleShape2D.new()
+	shape.radius = DETECT_RADIUS
+	$Visibility/CollisionShape2D.shape = shape
+	$ShootTimer.wait_time = FIRE_RATE
 
 func _physics_process(delta):
-	velocity.y += GRAVITY 
-	velocity = move_and_slide(velocity, Vector2(0, -1))
+	update()
+	if !is_on_floor():
+		velocity.y += GRAVITY 
+		velocity = move_and_slide(velocity, Vector2(0, -1))
+	
+	if target:
+		if can_shoot:
+			shoot(target.position)
+
+func _on_ShootTimer_timeout():
+	can_shoot = true
+
+func _on_Visibility_body_entered(body):
+	if target:
+		return
+	target = body
+
+func _on_Visibility_body_exited(body):
+	if body == target:
+		target = null
